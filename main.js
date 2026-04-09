33960de853468e82abab5975f1ec9a7a133351c4
paulinovart <pauline.dierick@gmail.com>
2026-04-09 11:10:27 +0200

    Update import path for OrbitControls

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //imports
 import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DragControls } from "three/addons/controls/DragControls.js";
